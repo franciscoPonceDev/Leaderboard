0ace64a8b4b3676f8130f2d27380768df782a01f
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-03-01 15:31:38 -0300

    add readme

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css';
+import Leaderboard from './modules/leaderboard.js';
+import User from './modules/user.js';
+
+const inputName = document.getElementById('name');
+const inputScore = document.getElementById('score');
+const submitBtn = document.querySelector('#submit');
+
+const leaderb = new Leaderboard();
+if (localStorage.getItem('Scores')) {
+  const localScores = JSON.parse(localStorage.getItem('Scores'));
+  localScores.scoreList.forEach((element) => {
+    leaderb.add(new User(element.name, element.score));
+  });
+}
+
+submitBtn.addEventListener('click', () => {
+  leaderb.add(new User(inputName.value, inputScore.value));
+  inputScore.value = '';
+  inputName.value = '';
+});
